411800900ca490b2dc48f37f919b2fb93b27ab8a
nkshn <vovanika23413@gmail.com>
2020-06-04 20:40:32 +0300

    add showing list of all links

--- client/src/pages/LinksPage.js
+++ client/src/pages/LinksPage.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useCallback, useContext, useEffect } from 'react';
+import { useHttp } from '../hooks/http.hook';
+import { AuthContext } from '../context/AuthContext';
+import { Loader } from '../components/Loader';
+import { LinksList } from '../components/LinksList';
 
 export const LinksPage = () => {
-  return (
-    <div>
-      <h3>Links page</h3>
-    </div>
-  );
+  const [links, setLinks] = useState([]);
+  const { loading, request } = useHttp();
+  const { token } = useContext(AuthContext);
+
+  const fetchLinks = useCallback(async () => {
+    try {
+      const fetched = await request('/api/link/', 'GET', null, {
+        Authorization: `Bearer ${token}`,
+      });
+      setLinks(fetched);
+    } catch (error) {}
+  }, [token, request]);
+
+  useEffect(() => {
+    fetchLinks();
+  }, [fetchLinks]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return <>{!loading && <LinksList links={links} />}</>;
 };
